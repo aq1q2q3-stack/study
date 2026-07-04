5b34d0eb628c6331b28fa89d0e39fa3837fd516d
aq1q2q3-stack <aq1q2q3@suwon.ac.kr>
2026-07-05 02:09:25 +0900

    atcoder

--- 465ABC/465ABC_A.cpp
+++ 465ABC/465ABC_A.cpp
@@ -5,8 +5,15 @@ using namespace std;
 int main()
 {
     cin.tie(0)->sync_with_stdio(false);
-    
+    int a,b; cin >> a >> b;
 
+    double answer = (b * 2) / 3;
 
+    if(a > answer) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
     return 0;
 }
--- 465ABC/465ABC_B.cpp
+++ 465ABC/465ABC_B.cpp
@@ -5,8 +5,22 @@ using namespace std;
 int main()
 {
     cin.tie(0)->sync_with_stdio(false);
-    
+    int x,y,L,R,a,b; cin >> x >> y >> L >> R >> a >> b;
 
+    int answer = 0;
+    int time = b - a;
+    //사이인 구간 구하기
+    
+    while(time--) {
+        if((L <= a && a <= R) &&(L <= a + 1 && a + 1 <= R)) {
+            answer += x;
+        }
+        else {
+            answer += y;
+        }
+        a++;
+    }  
+    cout << answer;
 
     return 0;
 }
